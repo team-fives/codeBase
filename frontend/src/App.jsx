31c435c7db6fad4fd7eb8a745e02b8a3d7f1b2e0
David <dcobos00@gmail.com>
2024-02-15 11:45:06 -0500

    articles nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import AboutUsPage from './pages/About-us';
 import MapPage from './pages/MapPage';
 import ArticlesPage from './pages/ArticlesPage';
 
+
 export default function App() {
   const { setCurrentUser } = useContext(UserContext);
   useEffect(() => {
@@ -32,11 +33,10 @@ export default function App() {
         <Route path='/users' element={<UsersPage />} />
         <Route path='/users/:id' element={<UserPage />} />
         <Route path='/posts' element={<CommunityPosts />} />
-        <Route path='/posts/:id' element={<PostPage/>} />
-        <Route path='/About-us' element={<AboutUsPage />} />
-        <Route path='/articles' element={<ArticlesPage />} />
         <Route path='/map' element={<MapPage/>} />
         <Route path='*' element={<NotFoundPage />} />
+        <Route path='/About-us' element={<AboutUsPage />} />
+        <Route path='/Articles' element={<ArticlesPage />} />
       </Routes>
     </main>
     <Footer />
