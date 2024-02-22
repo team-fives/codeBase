301df527daded1edddec564b710abdff9e81e8b4
Joeyyyys <josephgiler2@gmail.com>
2024-02-22 16:18:41 -0500

    added the date of event property into post table and deleted tags, updated the models and controllers and tested outthe create post controller on postman

--- src/controllers/post/update-post.js
+++ src/controllers/post/update-post.js
@@ -1,14 +1,14 @@
 const updatePost = async (req, res) => {
   const {
     db: { Post }, 
-    body: { title, description, location, image, start_time, end_time, tags }, 
+    body: { title, description, location, image, start_time, end_time, date_of_event }, 
     params: { id }, 
   } = req;
 
   const post = await Post.find(id);
   if (!post) return res.sendStatus(404);
 
-  const updatedPost = await Post.updatePost(title, description, location, image, id, start_time, end_time, tags);
+  const updatedPost = await Post.updatePost(title, description, location, image, id, start_time, end_time, date_of_event);
   res.send(updatedPost);
 };
 
