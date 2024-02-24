ba8d4c34ea78145cbc911172e357fdaf33c66719
Joeyyyys <josephgiler2@gmail.com>
2024-02-24 17:36:32 -0500

    added the date_of_event property in the update controller for post

--- src/controllers/post/update-post.js
+++ src/controllers/post/update-post.js
@@ -8,7 +8,7 @@ const updatePost = async (req, res) => {
   const post = await Post.find(id);
   if (!post) return res.sendStatus(404);
 
-  const updatedPost = await Post.updatePost(title, description, location, image, id, start_time, end_time, date_of_event);
+  const updatedPost = await Post.updatePost(title, description, location, image, id, start_time, end_time, date_of_event); // hasn't been tested 
   res.send(updatedPost);
 };
 
