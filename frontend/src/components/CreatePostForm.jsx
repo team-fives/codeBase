56570729fa23555485607816913a9248ed7183aa
David <dcobos00@gmail.com>
2024-02-29 11:41:29 -0500

    space

--- frontend/src/components/CreatePostForm.jsx
+++ frontend/src/components/CreatePostForm.jsx
@@ -57,7 +57,7 @@ export default function CreatePostForm({ posts, setPosts }) {
     setPicture('') //resets/clears input
     setdescription('')
     setEndTime('')
-      setDateOfEvent('');
+    setDateOfEvent('');
     setStartTime('')
 
 
